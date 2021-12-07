c7ec5a373df772fdf347929ee14771cc5e0e7482
Thom Versigny <thomversigny@gmail.com>
2021-12-07 09:54:49 +0100

    feature added

--- ijsjes.py
+++ ijsjes.py
@@ -1,26 +1,32 @@
-opnieuw='y'
+particulier='y'
+zakelijk='y'
 bakjestellen='.'
 toppingeidelijk=0
 aantalhorentjes=0
 aantalbakjes=0
 aantaleindelijk=0
-print("Welkom bij Papi Gelato")
 def vraagbolljes():
 	while True:
-		aantal = int(input("Hoeveel bolletjes wilt u? "))
-		if aantal == 1 or aantal == 2 or aantal == 3:
-			return aantal
-		elif aantal == 4 or aantal == 5 or aantal == 6 or aantal == 7 or aantal == 8:
-			print(f'Dan krijgt u van mij een bakje met {aantal} bolletjes')
-			return aantal
-		elif aantal > 8:
-			print("Sorry, zulke grote bakken hebben we niet")
-		else:
-			print("Sorry dat snap ik niet...")
+		aantal = int(input(f"Hoeveel {bolletjesORliter} wilt u? "))
+		if bolletjesORliter=='bolletje':
+			if aantal == 1 or aantal == 2 or aantal == 3:
+				return aantal
+			elif aantal == 4 or aantal == 5 or aantal == 6 or aantal == 7 or aantal == 8:
+				print(f'Dan krijgt u van mij een bakje met {aantal} {bolletjesORliter}')
+				return aantal
+			elif aantal > 8:
+				print("Sorry, zulke grote bakken hebben we niet")
+			else:
+				print("Sorry dat snap ik niet...")
+		elif bolletjesORliter=='liter':
+			if aantal >= 1:
+				return aantal
+			else:
+				print("Sorry dat snap ik niet...")
 def smaakkiezen():
 	Nbolletje = 1
 	while Nbolletje <= aantal:
-		smaak = input(f'Welke smaak wilt u voor bolletje nummer {Nbolletje}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?').lower()
+		smaak = input(f'Welke smaak wilt u voor {bolletjesORliter} nummer {Nbolletje}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?').lower()
 		if smaak=='a' or smaak=='c' or smaak=='m' or smaak=='v':
 			Nbolletje+=1
 		else:
@@ -63,44 +69,78 @@ def topping():
 			print('Sorry dat snap ik niet...')
 def again():
 	while True:
-		opnieuw = input(f'Hier is uw {bakje} met {aantal} bolletje(s). Wilt u nog meer bestellen? (Y/N)').lower()
-		if opnieuw == 'y':
-			return opnieuw
-		elif opnieuw =='n':
+		particulier = input(f'Hier is uw {bakje} met {aantal} bolletje(s). Wilt u nog meer bestellen? (Y/N)').lower()
+		if particulier == 'y':
+			return particulier
+		elif particulier =='n':
 			print('Bedankt en tot ziens!')
-			return opnieuw
+			return particulier
 		else:
 			print('Sorry, dat snap ik niet...')
 def bonnnetje():
-	if aantalbakjes>=1:
-		totalbakje=f'Bakje     {aantalbakjes} x €0,75 = €{round(aantalbakjes*0.75, 2)}'
-	else:
-		totalbakje=''
-	if aantalhorentjes>=1:
-		totalhoorntje=f'hoorntje  {aantalhorentjes} x €1,25 = €{round(aantalhorentjes*1.25, 2)}'
-	else:
-		totalhoorntje=''
-	if toppingeidelijk>=0.1:
-		totaltopping=f'topping    1 x €{toppingeidelijk} = €{toppingeidelijk}'
-	else:
-		totaltopping=''
-	totalbolletjes=f'Bolletjes {aantaleindelijk} x €1,10 = €{round(aantaleindelijk*1.10, 2)}'
-	totaalmoney=f'Totaal                €{round(aantalbakjes*0.75, 2)+round(aantaleindelijk*1.10, 2)+round(aantalhorentjes*1.25, 2)+round(toppingeidelijk, 2)}'
-	print(f"""---------["Papi Gelato"]---------
+	if particulier=='y'and zakelijk=='n':
+		if aantalbakjes>=1:
+			totalbakje=f'Bakje     {aantalbakjes} x €0,75 = €{round(aantalbakjes*0.75, 2)}'
+		else:
+			totalbakje=''
+		if aantalhorentjes>=1:
+			totalhoorntje=f'hoorntje  {aantalhorentjes} x €1,25 = €{round(aantalhorentjes*1.25, 2)}'
+		else:
+			totalhoorntje=''
+		if toppingeidelijk>=0.1:
+			totaltopping=f'topping    1 x €{toppingeidelijk} = €{toppingeidelijk}'
+		else:
+			totaltopping=''
+		totalbolletjes=f'Bolletjes {aantaleindelijk} x €1,10 = €{round(aantaleindelijk*1.10, 2)}'
+		totaalmoney=f'Totaal                €{round(aantalbakjes*0.75, 2)+round(aantaleindelijk*1.10, 2)+round(aantalhorentjes*1.25, 2)+round(toppingeidelijk, 2)}'
+		print(f"""---------["Papi Gelato"]---------
 
-        {totalbolletjes}
-        {totalhoorntje}
-        {totalbakje}
-        {totaltopping}
-                              ------+
-        {totaalmoney}""")
-while opnieuw=='y':
+	        {totalbolletjes}
+	        {totalhoorntje}
+	        {totalbakje}
+	        {totaltopping}
+	                              ------+
+	        {totaalmoney}""")
+	elif zakelijk=='y'and particulier=='n':
+		totalbolletjes=f'Liter     {aantaleindelijk} x €9,80 = €{round(aantaleindelijk*9.80, 2)}'
+		totaalmoney=f'Totaal              = €{round(aantaleindelijk*9.80, 2)}'
+		totalbtw=f'BTW (9%)            = €{round(aantaleindelijk*9.80/100*9, 2)}'
+		print(f"""---------["Papi Gelato"]---------
+
+	        {totalbolletjes}
+	                              ------+
+	        {totaalmoney}
+	        {totalbtw}""")
+#--------------------------------------------------------------------------------#
+print("Welkom bij Papi Gelato")
+vraagzakelijk = int(input('Bent u 1) particulier of 2) zakelijk? '))
+while True:
+	if vraagzakelijk==1:
+		particulier='y'
+		zakelijk='n'
+		bolletjesORliter='bolletje'
+		break
+	elif vraagzakelijk==2:
+		particulier='n'
+		zakelijk='y'
+		bolletjesORliter='liter'
+		break	
+	else:
+		print('Sorry dat snap ik niet...')
+if particulier=='y'and zakelijk=='n':
+	while particulier=='y'and zakelijk=='n':
+		while particulier=='y':
+			aantal = vraagbolljes()
+			smaakkiezen()
+			bakje = bakjeofhoorntje()
+			toppingkeuze=topping()
+			particulier = again()
+			toppingeidelijk+=toppingkeuze
+			aantaleindelijk+=aantal
+	bonnnetje()
+else:
 	aantal = vraagbolljes()
 	smaakkiezen()
-	bakje = bakjeofhoorntje()
-	toppingkeuze=topping()
-	opnieuw = again()
-	toppingeidelijk+=toppingkeuze
 	aantaleindelijk+=aantal
-bonnnetje()
+	bonnnetje()
 input("press enter to exit")
